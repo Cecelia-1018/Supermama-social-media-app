705471949a284b0f30c7336ef1ee72acce05c09d
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2022-01-21 17:01:20 +0800

    Update ProfileScreen.js

--- Supermama/components/UserProfile/ProfileScreen.js
+++ Supermama/components/UserProfile/ProfileScreen.js
@@ -1,54 +1,88 @@
-import React,{useState} from 'react';
-import {View, Text, StyleSheet,StatusBar,Alert, Image} from 'react-native';
+import React, {useState,useCallback} from 'react';
+import {View, RefreshControl,Linking, Text, StyleSheet, StatusBar, Alert, Image} from 'react-native';
 import {ImageOrVideo} from 'react-native-image-crop-picker';
 import {Avatar} from './Avatar';
-import { utils } from '@react-native-firebase/app';
-import storage from '@react-native-firebase/storage'; 
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {utils} from '@react-native-firebase/app';
+import storage from '@react-native-firebase/storage';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import VerifyProScreen from '../VerifyPro/VerifyProScreen';
 import LogOut from './LogOut';
 import auth, {firebase} from '@react-native-firebase/auth';
-import {Button,Card, IconButton, Title} from 'react-native-paper';
+import {Button, Card, IconButton, Title} from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 const Drawer = createDrawerNavigator();
+const wait = (timeout) => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
 
-function GuestProfile({navigation}){
-  return(
-   <View style={{ flex: 1,alignItems: 'center', justifyContent: 'center',backgroundColor: 'white' }}>
-   
-   <Title>Hey, get your sign in!</Title>
-   <Button> Go Sign In </Button>
-      <Image
-        style={styles.tinyLogo}
-        source={require('./Mobile-login.jpg')}
-      />
-      <Text>Designed by  / Freepik</Text>
+function GuestProfile() {
+  const navigation = useNavigation();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Title>Hey, get your sign in!</Title>
+      <Button
+        onPress={() => {
+          navigation.navigate('SigninScreen');
+        }}
+        color="#FE7E9C">
+        Go Sign In{' '}
+      </Button>
+      {/* <Button
+        onPress={() => {
+         navigation.navigate('Profile');
+        }}
+        color="#FE7E9C">
+        Press if you signed in.{' '}
+      </Button> */}
+      <Image style={styles.tinyLogo} source={require('./Mobile-login.jpg')} />
+      <Text style={{color: 'blue'}}
+      onPress={() => Linking.openURL('http://www.freepik.com')}>
+      Designed by  / Freepik
+      </Text>
     </View>
   );
 }
 
-function Profile(){
  const onAvatarChange = (image: ImageOrVideo) => {
     console.log(image);
-    
+
     // user id
     let userId = 'U002';
 
     // upload image to server here
-    let reference = storage().ref('gs://supermama-6aa87.appspot.com/' + userId);   //2
+    let reference = storage().ref('gs://supermama-6aa87.appspot.com/' + userId); //2
     let task = reference.putFile(image.path.toString());
 
-    task.then(() =>{
+    task
+      .then(() => {
         console.log('Image uploaded to the bucket!');
-    }).catch((e) => console.log('uploading image error =>', e));
-
-   
+      })
+      .catch(e => console.log('uploading image error =>', e));
   };
 
- const user = firebase.auth().currentUser;
- if(user) {
-   return (
-     <View style={styles.scroll}>
+function ProfileInfo(){
+  
+   firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log('user logged');
+      
+      }
+   });
+const user = firebase.auth().currentUser;
+return (
+      <View style={styles.scroll}>
+      {/* <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          /> */}
       <StatusBar barStyle="dark-content" />
       <View style={styles.userRow}>
         <Avatar
@@ -60,30 +94,65 @@ function Profile(){
       </View>
       <View style={styles.content} />
     </View>
-   
-   );
- }
- return (
-   <GuestProfile/>
- );
-
+    );
+  
 }
 
-function ProfileScreen( ) {
+class Profile extends React.Component{
+   
+  //  const [refreshing, setRefreshing] = useState("");
+  //  const onRefresh = React.useCallback(() => {
+  //   setRefreshing(true);
+  //   wait(2000).then(() => setRefreshing(false));
+  // }, []);
+  
+  render() {
+  
+  const user = firebase.auth().currentUser;
+  if (user) {
+    return <ProfileInfo/>;
+  }else {
+    return <GuestProfile />;
+  }
+  
+}
+}
+
+
+function ProfileScreen() {
   return (
-  <Drawer.Navigator>
-    <Drawer.Screen name="User Profile" component={Profile} />
-    <Drawer.Screen name="Apply Verify Professional" component={VerifyProScreen} />
-    <Drawer.Screen name="Log Out" component={LogOut} />
-  </Drawer.Navigator>
-    
- );
- 
-};
+    <Drawer.Navigator>
+      <Drawer.Screen name="User Profile" component={Profile} />
+      <Drawer.Screen
+        name="Apply Verify Professional"
+        component={VerifyProScreen}
+      />
+      <Drawer.Screen name="Log Out" component={LogOut} />
+    </Drawer.Navigator>
+  );
+}
 
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 0,
+    flexDirection: "column",
+    backgroundColor: 'white',
+  },
+  leftItem: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: "auto",
+  },
+  rightItem: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: 200,
+    paddingLeft: 25,
+    paddingTop: 15,
+  },
   scroll: {
     backgroundColor: 'white',
     flex: 1,
