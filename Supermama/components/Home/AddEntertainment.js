fd9d2cf8d546ea3b8957ecf69908af2143404f81
WaiYi1612 <76613496+WaiYi1612@users.noreply.github.com>
2022-01-25 16:07:08 +0800

    1/2 Add Entertainment Post

--- Supermama/components/Home/AddEntertainment.js
+++ Supermama/components/Home/AddEntertainment.js
@@ -1,21 +1,143 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, TextInput, Alert} from 'react-native';
+import {Card, Button} from 'react-native-paper';
+import firestore from '@react-native-firebase/firestore';
+import {Icon} from 'react-native-elements';
+import Feather from 'react-native-vector-icons/Feather';
 
-const AddEntertainment = () => {
+import EntertainmentHome from './EntertainmentHome';
+function AddEntertainment({navigation}) {
+  const [txtHashtag, setTxtHashtag] = useState('');
+  const [txtDes, setTxtDes] = useState('');
+  //image
+
+  const ref = firestore().collection('entertainment');
+
+  const [entId, setEntId] = useState('');
+  const [entDocId, setEntDocId] = useState('');
+
+  useEffect(() => {
+    var date = Date.now().toString();
+    var hours = new Date().getHours(); //To get the Current Hours
+    var min = new Date().getMinutes(); //To get the Current
+    var sec = new Date().getSeconds(); //To get the Current Seconds
+
+    setEntId('E' + date + hours + min + sec);
+    setEntDocId('E' + date + hours + min + sec);
+  }, []);
+
+  async function addEntertainmentCol() {
+    await ref
+      .doc(entDocId)
+      .set({
+        //add id here
+        entertainmentId: entId,
+        description: txtDes,
+        hashtag: txtHashtag,
+      })
+      .then(() => {
+        Alert.alert('Success ✅', 'Post Added Success');
+      });
+    setTxtHashtag('');
+    setTxtDes('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>AddEntertainment</Text>
-      <Button title="Click Here" onPress={() => alert('Button clicked')} />
+      <View style={styles.header}>
+        <Text style={styles.text_header}>Picture And Words</Text>
+        <Text style={styles.header_text}>
+          Share to people who know about you
+        </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.text_footer}>Username</Text>
+        <Text style={[styles.text_footer, {marginTop: 20}]}>Hashtag</Text>
+        <View style={styles.action}>
+          <Feather name="hash" color="grey" size={25} style={{marginTop: 10}} />
+          <TextInput
+            label="Hashtag"
+            value={txtHashtag}
+            onChangeText={setTxtHashtag}
+          />
+        </View>
+        <Text style={[styles.text_footer, {marginTop: 10}]}>Description</Text>
+        <View style={styles.action}>
+          <TextInput
+            label="Description"
+            value={txtDes}
+            onChangeText={setTxtDes}
+            multiline={true}
+            numberOfLines={3}
+          />
+        </View>
+        <Text style={[styles.text_footer, {marginTop: 10}]}>Image</Text>
+        <View style={styles.button}>
+          <Button
+            mode="outlined"
+            onPress={() => {
+              addEntertainmentCol();
+            }}
+            color="#FE7E9C">
+            Submit
+          </Button>
+        </View>
+      </View>
     </View>
   );
-};
+}
 
 export default AddEntertainment;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFC0CB',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 30,
+  },
+  footer: {
+    flex: 5,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  header_text: {
+    color: 'grey',
+    marginTop: 5,
+  },
+  text_footer: {
+    color: '#05375a',
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 8,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -10,
+    paddingLeft: 12,
+    paddingBottom: 2,
+    color: '#05375a',
+  },
+  button: {
+    marginTop: 30,
+    justifyContent: 'space-evenly',
   },
 });
