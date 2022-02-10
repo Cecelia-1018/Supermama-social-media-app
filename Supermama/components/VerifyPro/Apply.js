2ccb0e81b765bcf54965e9e5891684ccfde58164
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2022-02-10 17:51:55 +0800

    Done Picture Part

--- Supermama/components/VerifyPro/Apply.js
+++ Supermama/components/VerifyPro/Apply.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
 
+// * Step 1 : Import 
+import {ImageOrVideo} from 'react-native-image-crop-picker';
+import {Certificate} from './Certificate';  // rmb to create a Certificate js and copy the code from avatar.js so that you may resize you image
+import storage from '@react-native-firebase/storage';
+import auth, {firebase} from '@react-native-firebase/auth';
+
+// * Step 2 : Write a image change
+const onImageChange = (image: ImageOrVideo) => {
+  console.log(image);
+
+  const user = firebase.auth().currentUser;
+
+  let Id = "Verify" + user.uid;
+
+  //* step 2 a : upload image to storage
+  let reference = storage().ref('gs://supermama-6aa87.appspot.com/VerifyPro/' + Id); //2
+  let task = reference.putFile(image.path.toString());
+
+  task
+    .then(() => {
+      console.log('Image uploaded to the bucket!');
+    })
+    .catch(e => console.log('uploading image error =>', e));
+};
+
 const Apply = () => {
+
+  const user = firebase.auth().currentUser;
+
+  // * step 3 declare picture url for displaying (rmb import useState at top)
+  const [imageUrl, setImageUrl] = useState(undefined);
+  
+  // * step 3a call image from storage (rmb import useEffect at top)
+  useEffect(() => {
+    storage()
+      .ref('gs://supermama-6aa87.appspot.com/VerifyPro/' + "Verify" +user.uid) //name in storage in firebase console
+      .getDownloadURL()
+      .then((url) => {
+        setImageUrl(url);
+      })
+      .catch((e) => console.log('Errors while downloading => ', e));
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Text>ChatScreen</Text>
+        {/* * step 4 place image change at screen */}
+       <Certificate onChange={onImageChange} source={imageUrl? {uri: imageUrl} : require('../UserProfile/addimg.png')} />
+        {imageUrl ?  null : <Text>Press image to upload photo.</Text>}
       <Button title="Click Here" onPress={() => alert('Button clicked')} />
     </View>
   );
