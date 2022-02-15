688c3fb1a5a663203728cc442308939caf74589c
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2022-02-15 16:36:04 +0800

    Update SearchScreen.js

--- Supermama/components/FilterSearch/SearchScreen.js
+++ Supermama/components/FilterSearch/SearchScreen.js
@@ -1,18 +1,154 @@
 import React, {useState, useEffect} from 'react';
 import {Searchbar} from 'react-native-paper';
+import firestore from '@react-native-firebase/firestore';
+
+// step 1 : import all the components we are going to use
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  TextInput,
+  Alert,
+} from 'react-native';
+
 
 function SearchScreen() {
-  const [searchQuery, setSearchQuery] = React.useState('');
 
-  const onChangeSearch = query => setSearchQuery(query);
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
 
-  return (
-    <Searchbar
-      placeholder="Search"
-      onChangeText={onChangeSearch}
-      value={searchQuery}
-    />
+   useEffect(() => {
+    const subscriber = firestore()
+      .collection('forums')
+      .onSnapshot(
+        querySnapshot => {
+          const forums = [];
+
+          querySnapshot.forEach(documentSnapshot => {
+            forums.push({
+              ...documentSnapshot.data(),
+              key:documentSnapshot.id
+            });
+          });
+    
+          
+         setFilteredDataSource(forums);
+         setMasterDataSource(forums);
+          
+
+        });
+    
+
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource
+      // Update FilteredDataSource
+      const newData = masterDataSource.filter(
+        function (item) {
+          const itemData = item.title
+            ? item.title.toUpperCase()
+            : ''.toUpperCase();
+          const textData = text.toUpperCase();
+          return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({item}) => {
+    return (
+      // Flat List Item
+      <Text
+        style={styles.itemStyle}
+        onPress={() => getItem(item)}>
+        {item.forumId}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+   const getItem = (item) => {
+    // Function for click on an item
+    alert('Id : ' + item.forumId + ' Title : ' + item.title);
+  };
+
+ return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text) => searchFilterFunction(text)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Search Here"
+        />
+        <FlatList
+          data={filteredDataSource}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+        />
+      </View>
+    </SafeAreaView>
   );
+
+
+  // const [searchQuery, setSearchQuery] = React.useState('');
+
+  // const onChangeSearch = query => setSearchQuery(query);
+
+  // return (
+  //   <Searchbar
+  //     placeholder="Search"
+  //     onChangeText={onChangeSearch}
+  //     value={searchQuery}
+  //   />
+  // );
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+  itemStyle: {
+    padding: 10,
+  },
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF',
+  },
+});
 
 export default SearchScreen;
