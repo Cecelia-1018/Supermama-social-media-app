1615b3dbedc67769fb16f25b4b2829d5b854c183
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2022-03-23 23:43:30 +0800

    Update SearchPostScreen.js

--- Supermama/components/FilterSearch/SearchPostScreen.js
+++ Supermama/components/FilterSearch/SearchPostScreen.js
@@ -105,8 +105,6 @@ function SearchEntertainments({navigation}) {
         
       </Text>
       </View>
-       
-
       </View> 
     );
   };
@@ -146,8 +144,128 @@ function SearchEntertainments({navigation}) {
   );
 }
 
-function SearchFeeds() {
-  return <Text>Feed</Text>;
+function SearchFeeds({navigation}) {
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+  
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('feed')
+      .onSnapshot(
+        querySnapshot => {
+          const feed = [];
+
+          querySnapshot.forEach(documentSnapshot => {
+            feed.push({
+              ...documentSnapshot.data(),
+              key:documentSnapshot.id
+            });
+          });
+    
+          
+         setFilteredDataSource(feed);
+         setMasterDataSource(feed);
+          
+
+        });
+    
+
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource
+      // Update FilteredDataSource
+      const newData = masterDataSource.filter(
+        function (item) {
+          const itemData = item.hashtag
+            ? item.hashtag.toUpperCase()
+            : ''.toUpperCase();
+          const textData = text.toUpperCase();
+          return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({item}) => {
+    return (
+      // Flat List Item
+      <View style={{flexDirection: 'row', margin: 10}}> 
+      <View style={styles.box2}> 
+        <Image style={styles.image} source={{uri: item.image}} />
+      </View>
+
+      <View style={styles.box}> 
+      <Text
+        style={styles.itemStyle}
+        onPress={() => {
+          navigation.navigate('Feed Detail', {
+            item: {
+              title: item.title,
+              description: item.description,
+              feedId: item.feedId,
+              details: item.details,
+              hashtag: item.hashtag,
+              userid: item.userId,
+              username: item.username,
+              image: item.image,
+            },
+          });
+        }}
+          >
+        #{item.hashtag}  {'\n'}
+        {item.description}
+        
+      </Text>
+      </View>
+      </View> 
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+
+  return(
+  <SafeAreaView style={{flex: 1}}>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInputStyle}
+        onChangeText={(text) => searchFilterFunction(text)}
+        value={search}
+        underlineColorAndroid="transparent"
+        placeholder="Search By Hashtag"
+      />
+      <FlatList
+        data={filteredDataSource}
+        keyExtractor={(item, index) => index.toString()}
+        ItemSeparatorComponent={ItemSeparatorView}
+        renderItem={ItemView}
+      />
+    </View>
+  </SafeAreaView>);
 }
 
 function SearchPostScreen() {
@@ -175,6 +293,7 @@ const styles = StyleSheet.create({
   },
   itemStyle: {
     padding: 10,
+    fontWeight: "bold"
   },
   textInputStyle: {
     height: 40,
