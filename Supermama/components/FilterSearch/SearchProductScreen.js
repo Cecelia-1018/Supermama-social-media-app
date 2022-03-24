f548077d983238e17a7cacefb78223d1f3ca08af
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2022-03-24 15:53:30 +0800

    Update SearchProductScreen.js

--- Supermama/components/FilterSearch/SearchProductScreen.js
+++ Supermama/components/FilterSearch/SearchProductScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect,useRef} from 'react';
-import {Searchbar} from 'react-native-paper';
+import {Searchbar,Paragraph,Title} from 'react-native-paper';
 import firestore from '@react-native-firebase/firestore';
 // step 1 : import all the components we are going to use
 import {
@@ -163,11 +163,297 @@ function SearchByName({navigation}) {
 }
 
 function SearchByPrice({navigation}) {
-    return <Text>Price</Text>;
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+  const flatlistRef = useRef();
+
+  const onPressFunction = () => {
+  flatlistRef.current.scrollToEnd({animating: true});
+  };
+
+  const [product, setProduct] = useState([]);
+
+     useEffect(() => {
+        const subscriber = firestore()
+          .collection('product')
+          .onSnapshot(
+            querySnapshot => {
+              const product= [];
+    
+              querySnapshot.forEach(documentSnapshot => {
+                product.push({
+                  ...documentSnapshot.data(),
+                  key:documentSnapshot.id
+                });
+              });
+        
+              
+             setFilteredDataSource(product);
+             setMasterDataSource(product);
+             setProduct(product);
+    
+            });
+        
+    
+        // Unsubscribe from events when no longer in use
+        return () => subscriber();
+      }, []);
+
+      const searchFilterFunction = (text) => {
+        // Check if searched text is not blank
+        if (text) {
+          // Inserted text is not blank
+          // Filter the masterDataSource
+          // Update FilteredDataSource
+          const newData = masterDataSource.filter(
+            function (item) {
+              const itemData = item.price
+                ? item.price.toUpperCase()
+                : ''.toUpperCase();
+              const textData = text.toUpperCase();
+              return itemData.indexOf(textData) > -1;
+          });
+          setFilteredDataSource(newData);
+          setSearch(text);
+        } else {
+          // Inserted text is blank
+          // Update FilteredDataSource with masterDataSource
+          setFilteredDataSource(masterDataSource);
+          setSearch(text);
+        }
+      };
+    
+      const ItemView = ({item}) => {
+        return (
+          // Flat List Item
+          <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Product Details', {
+              item: {
+                category: item.category,
+                description: item.description,
+                imageUrl: item.image,
+                name: item.name,
+                price: item.price,
+                userId: item.userId,
+                username: item.username,
+                productId: item.productId,
+              },
+            });
+          }}
+          style={{borderRadius: 10, marginRight: 3, marginLeft: 5, marginBottom: 5}}
+          >
+             <LinearGradient
+        colors={['#DAE2F8', '#ffdde1' ]}
+        // style={styles.box1}
+        start={{x: 0.3, y: 0}}
+        style={{borderRadius: 5,}}
+        >
+           <View style={{alignItems: 'center'}}>
+         <Title>RM {item.price}</Title>
+         </View>
+          <View style={styles.gridStyle}> 
+       
+          <View> 
+            <Image style={styles.image} source={{uri: item.image}} />
+          </View>
+    
+          <View> 
+          <Text style={styles.itemStyle}>{item.name} {'\n'}Category: {item.category}</Text>
+          </View>
+         
+          </View>
+          </LinearGradient>
+          </TouchableOpacity> 
+        );
+      };
+    
+      const ItemSeparatorView = () => {
+        return (
+          // Flat List Item Separator
+          <View
+            style={{
+              height: 0.5,
+              width: '100%',
+              backgroundColor: '#C8C8C8',
+            }}
+          />
+        );
+      };
+    
+
+    return (
+        <SafeAreaView style={{flex: 1}}>
+        <View style={styles.container}>
+          <TextInput
+            style={styles.textInputStyle}
+            onChangeText={(text) => searchFilterFunction(text)}
+            value={search}
+            underlineColorAndroid="transparent"
+            placeholder="Search By Price"
+          />
+          <FlatList
+            data={filteredDataSource}
+            keyExtractor={(item, index) => index.toString()}
+            // ItemSeparatorComponent={ItemSeparatorView}
+            renderItem={ItemView}
+            numColumns={2}
+            ref={flatlistRef}
+            windowSize={5}
+            initialNumToRender={product.length}
+            maxToRenderPerBatch={product.length}
+          />
+        </View>
+      </SafeAreaView>
+    );
 }
 
 function SearchByCategory({navigation}) {
-    return <Text>Category</Text>;
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+  const flatlistRef = useRef();
+
+  const onPressFunction = () => {
+  flatlistRef.current.scrollToEnd({animating: true});
+  };
+
+  const [product, setProduct] = useState([]);
+
+     useEffect(() => {
+        const subscriber = firestore()
+          .collection('product')
+          .onSnapshot(
+            querySnapshot => {
+              const product= [];
+    
+              querySnapshot.forEach(documentSnapshot => {
+                product.push({
+                  ...documentSnapshot.data(),
+                  key:documentSnapshot.id
+                });
+              });
+        
+              
+             setFilteredDataSource(product);
+             setMasterDataSource(product);
+             setProduct(product);
+    
+            });
+        
+    
+        // Unsubscribe from events when no longer in use
+        return () => subscriber();
+      }, []);
+
+      const searchFilterFunction = (text) => {
+        // Check if searched text is not blank
+        if (text) {
+          // Inserted text is not blank
+          // Filter the masterDataSource
+          // Update FilteredDataSource
+          const newData = masterDataSource.filter(
+            function (item) {
+              const itemData = item.category
+                ? item.category.toUpperCase()
+                : ''.toUpperCase();
+              const textData = text.toUpperCase();
+              return itemData.indexOf(textData) > -1;
+          });
+          setFilteredDataSource(newData);
+          setSearch(text);
+        } else {
+          // Inserted text is blank
+          // Update FilteredDataSource with masterDataSource
+          setFilteredDataSource(masterDataSource);
+          setSearch(text);
+        }
+      };
+    
+      const ItemView = ({item}) => {
+        return (
+          // Flat List Item
+          <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Product Details', {
+              item: {
+                category: item.category,
+                description: item.description,
+                imageUrl: item.image,
+                name: item.name,
+                price: item.price,
+                userId: item.userId,
+                username: item.username,
+                productId: item.productId,
+              },
+            });
+          }}
+          style={{borderRadius: 10, marginRight: 3, marginLeft: 5, marginBottom: 5}}
+          >
+             <LinearGradient
+        colors={['#DAE2F8', '#ffdde1' ]}
+        // style={styles.box1}
+        start={{x: 0.3, y: 0}}
+        style={{borderRadius: 5,}}
+        >
+          <View style={{alignItems: 'center'}}>
+         <Title>{item.category}</Title>
+         </View>
+          <View style={styles.gridStyle}> 
+       
+          <View> 
+            <Image style={styles.image} source={{uri: item.image}} />
+          </View>
+    
+          <View> 
+          <Text style={styles.itemStyle}>{item.name} {'\n'}RM {item.price}</Text>
+          </View>
+         
+          </View>
+          </LinearGradient>
+          </TouchableOpacity> 
+        );
+      };
+    
+      const ItemSeparatorView = () => {
+        return (
+          // Flat List Item Separator
+          <View
+            style={{
+              height: 0.5,
+              width: '100%',
+              backgroundColor: '#C8C8C8',
+            }}
+          />
+        );
+      };
+    
+
+    return (
+        <SafeAreaView style={{flex: 1}}>
+        <View style={styles.container}>
+          <TextInput
+            style={styles.textInputStyle}
+            onChangeText={(text) => searchFilterFunction(text)}
+            value={search}
+            underlineColorAndroid="transparent"
+            placeholder="Search By Categories"
+          />
+          <FlatList
+            data={filteredDataSource}
+            keyExtractor={(item, index) => index.toString()}
+            // ItemSeparatorComponent={ItemSeparatorView}
+            renderItem={ItemView}
+            numColumns={2}
+            ref={flatlistRef}
+            windowSize={5}
+            initialNumToRender={product.length}
+            maxToRenderPerBatch={product.length}
+          />
+        </View>
+      </SafeAreaView>
+    );
 }
 
 function SearchProductScreen(){
