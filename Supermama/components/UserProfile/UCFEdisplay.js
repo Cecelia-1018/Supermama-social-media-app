cfb6eb743f05a170546e5fb02140a930795d42ae
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2022-03-28 01:01:44 +0800

    Update UCFEdisplay.js

--- Supermama/components/UserProfile/UCFEdisplay.js
+++ Supermama/components/UserProfile/UCFEdisplay.js
@@ -21,7 +21,150 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import auth, {firebase} from '@react-native-firebase/auth';
 
 function UCFEdisplay(){
-    return <Text>UCFEdisplay</Text>;
+   //check user
+   const user = firebase.auth().currentUser;
+   const flatlistRef = useRef();
+ 
+   const onPressFunction = () => {
+     flatlistRef.current.scrollToEnd({animating: true});
+   };
+ 
+   const [loading, setLoading] = useState(true); // Set loading to true on component mount
+   const [bookmark, setBookmark] = useState([]); // Initial empty array of forums
+   const [size, setSize] = useState('');
+ 
+   const renderItem2 = ({item}) => {
+     // //firebase reference
+   const ref = firestore()
+   .collection('bookmark')
+   .doc(user.uid)
+   .collection('userMarkFeed');
+ 
+     async function deleteBook() {
+       ref
+         .doc(item.feedId)
+         .delete()
+         .then(() => {
+           console.log('Feed bookmark deleted!');
+         });
+     }
+     return(
+      <SafeAreaProvider>
+      <TouchableOpacity
+         onPress={() => {
+           navigation.navigate('Your Collect Feeds', {
+             //pass params here
+             item: {
+               feedId: item.feedId,
+             },
+           });
+         }}>
+         <View>
+        
+           <Card>
+             <Card.Content>
+                <Paragraph>Saved - {item.title} </Paragraph>
+                <View style={styles.delete}>
+         <IconButton
+           color="#FE7E9C"
+           size={20}
+           icon={require('../Forum/delete-bin.png')}
+           onPress={() =>
+             Alert.alert('Confirmation', 'Confirm to delete?', [
+               {
+                 text: 'Cancel',
+                 onPress: () => console.log('Cancel Pressed'),
+                 style: 'cancel',
+               },
+               {
+                 text: 'Confirm',
+                 onPress: () => {
+                   deleteBook()
+                 },
+               },
+             ])
+           }
+         />
+       </View>
+             </Card.Content>
+           </Card>
+           </View>
+             </TouchableOpacity>
+         </SafeAreaProvider>
+   );
+ };
+
+
+
+ useEffect(() => {
+   const subscriber = firestore()
+     .collection('bookmark')
+     .doc(user.uid)
+     .collection('userMarkFeed')
+     .onSnapshot(
+       querySnapshot => {
+         const bookmark = [];
+        
+
+         querySnapshot.forEach(documentSnapshot => {
+         
+           bookmark.push({
+             ...documentSnapshot.data(),
+             key:documentSnapshot.id
+           });
+
+          
+           //start at here 
+           console.log('ID: ', documentSnapshot.id);
+          
+           
+         });
+   
+         
+         setBookmark(bookmark);
+         setLoading(false);
+         setSize(querySnapshot.size);
+         
+         
+       });
+   
+
+   // Unsubscribe from events when no longer in use
+   return () => subscriber();
+ }, []);
+
+
+ if (loading) {
+   return <ActivityIndicator size="large" color="#FFC0CB" />;
+ }
+
+
+
+    return (
+      <View style={styles.container}>
+        <Title style={styles.word}>Total Bookmarks : {size}</Title>
+        <FlatList
+          ref={flatlistRef}
+          data={bookmark}
+          keyExtractor={item => item.feedId}
+          renderItem={renderItem2}
+        />
+      </View>
+    );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  word:{
+    margin: 10,
+    padding: 5,
+  },
+  delete: {
+    marginLeft: -10,
+  },
+ 
+});
+
 
 export default UCFEdisplay;
